c216d939e45a22253c5530c5c9bb5c6195817eba
yagnik <yagnikdevani00@gmail.com>
2023-07-19 17:40:18 +0530

    wd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
-import './App.css';
-import Deepcopy from './Components/Deepcopy';
-import Shallowcopy from './Components/Shallowcopy';
+import "./App.css";
+import Deepcopy from "./Components/Deepcopy";
+import Shallowcopy from "./Components/Shallowcopy";
 
 function App() {
-  return (<>
-  <div className="fluid-container">
-    {/* <h1 className='text-center'>Deep copy</h1> */}
-    <Deepcopy/>
-    <br />
-    {/* <h1  className='text-center'>Shallow copy</h1> */}
-    <Shallowcopy/>
-    </div>
+  return (
+    <>
+      <div className="fluid-container">
+        {/* <h1 className='text-center'>Deep copy</h1> */}
+        <Deepcopy />
+        <br />
+        {/* <h1  className='text-center'>Shallow copy</h1> */}
+        <Shallowcopy />
+      </div>
     </>
   );
 }
